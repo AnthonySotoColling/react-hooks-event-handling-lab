2c7c2b263c7d0d5515fa558dc6b7e077b3a121bf
anthonyLuisSotoColling <govips@gmail.com>
2023-06-14 17:30:08 -0700

    ebents

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,12 @@
 // Code EyesOnMe Component Here
+
+function EyesOnMe() {
+    function output() {
+        console.log('Good!')
+    }
+    function trigger() {
+        console.log('Hey! Eyes on me!')
+    }
+    return <button onFocus={output} onBlur={trigger} >"Eyes on me"</button>
+}
+export default EyesOnMe
